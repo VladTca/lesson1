0baaa5156686919906bdb62d31fe59e0e40df088
VladTca <votsats@gmail.com>
2025-05-06 10:34:57 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Les1 from './lessons/les1/les1.jsx'
+
+function App() {
+
+  return (
+    <>
+<Les1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/lessons/les1/Les1.jsx
@@ -0,0 +1,14 @@
+import reactLogo from '../../assets/react.svg'
+import './Les1.css'
+
+function Les1() {
+    return (
+        <div className="react-container">
+            <img src="/vite.svg" alt="" />
+            <img src={reactLogo} alt="" />
+            <h1>Hello, React ⚡️</h1>
+        </div>
+    )
+}
+
+export default Les1;
