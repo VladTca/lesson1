991cce3562240fd3e3542b17daa13015cd049097
VladTca <votsats@gmail.com>
2025-05-31 00:43:53 +0200

    **feat(lesson17): add styles and improve structure for Lesson17**
    Добавлен файл стилей `lesson17.module.css` для лучшего оформления компонента `Lesson17`. 🎨
    Изменена структура JSX: добавлены классы и обработка стилей для элементов, чтобы улучшить читаемость, визуальную композицию и переиспользуемость. 🛠️
    Обновленные стили включают центрирование содержимого, акцентные цвета заголовков и отступы для элементов внутри. ✨

--- src/lessons/lesson17/Lesson17.tsx
+++ src/lessons/lesson17/Lesson17.tsx
@@ -1,20 +1,22 @@
-// import styles from './Lesson17.module.css'
+import styles from './lesson17.module.css'
 import type {JSX} from 'react';
 import React from 'react';
 import ChildrenProps from "../../components/childrenProps/ChildrenProps";
 
 export default function Lesson17():JSX.Element {
     return (
-        <div>
-            <h2>Lesson 17: Context + children practice</h2>
-            <ChildrenProps>
-                <h4>Эти данные переданы через children props 🙇‍♂️</h4>
-            </ChildrenProps>
-            <ChildrenProps>
-                <h2>Это карточка 🙆‍♂️</h2>
-                <p>Но все ее данные пришли через children props</p>
-                <h3>Такая карточка может быть более универсальной в использовании, т.к кол-во элементов HTML не ограничено</h3>
-            </ChildrenProps>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Lesson 17: Context + children practice</h2>
+            <div className={styles.childrenContainer}>
+                <ChildrenProps>
+                    <h4>Эти данные переданы через children props 🙇‍♂️</h4>
+                </ChildrenProps>
+                <ChildrenProps>
+                    <h2>Это карточка 🙆‍♂️</h2>
+                    <p>Но все ее данные пришли через children props</p>
+                    <h3>Такая карточка может быть более универсальной в использовании, т.к кол-во элементов HTML не ограничено</h3>
+                </ChildrenProps>
+            </div>
         </div>
     )
 }
